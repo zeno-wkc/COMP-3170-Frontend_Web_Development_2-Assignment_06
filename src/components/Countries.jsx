fdbe22723e16f038665945a6f490b3ab8edb43ba
Zeno <zenowkc@gmail.com>
2024-11-09 18:43:23 -0800

    final version 1.0.4

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,8 +1,8 @@
 import Country from './Country';
 
 export default function Countries({ countries, fetchStatus, getBorderNames, getCurrencyInfo }) {
-  if (fetchStatus === 'loading') return <p>Loading...</p>;
-  if (fetchStatus === 'error') return <p>Error loading data.</p>;
+  if (fetchStatus === 'loading') return <p className='message'>Loading...</p>;
+  if (fetchStatus === 'error') return <p className='message'>Error loading data.</p>;
   return (
     <div className='country_content__container'>
       {countries.map((country, index) => (
